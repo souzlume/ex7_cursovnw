e3df788e0b408606bb88407a7898c85bd5a4b4d9
souzlume <lucassmelo2020@gmail.com>
2025-11-24 19:37:31 -0300

    40% finalizado!

--- src/components/Lancamentos/index.jsx
+++ src/components/Lancamentos/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import './lancamentos.scss';
 
 function Lancamentos() {
--- src/components/Looks/index.jsx
+++ src/components/Looks/index.jsx
@@ -1,15 +1,57 @@
-import './looks.scss';
-import labio from '../../assets/labio.png';
-import olhos from '../../assets/olhos.png';
-import rosto from '../../assets/rosto.png';
-import tendencia from '../../assets/tendencia.png';
+import "./looks.scss";
+import labios from "../../assets/labios.png";
+import olhos from "../../assets/olhos.jpg";
+import rosto from "../../assets/rosto.png";
+import tendencia from "../../assets/tendencia.png";
 
 function Looks() {
-    return (
-        <>
-            
-        </>
-    );
+  return (
+    <>
+      <h1>LOOKS E DICAS DE MAQUIAGEM</h1>
+      <section className="looks">
+        <div className="cards">
+          <div className="cards-btn">
+            <img
+              className="cards-img"
+              src={labios}
+              alt="Mulher passando gloss"
+            />
+            <span className="subtitles">LÁBIOS</span>
+          </div>
+        </div>
+        <div className="cards">
+          <div className="cards-btn">
+            <img
+              className="cards-img"
+              src={olhos}
+              alt="Olhos com glitter dourado"
+            />
+            <span className="subtitles">OLHOS</span>
+          </div>
+        </div>
+        <div className="cards">
+          <div className="cards-btn">
+            <img
+              className="cards-img"
+              src={rosto}
+              alt="Mulher passando blush"
+            />
+            <span className="subtitles">ROSTO</span>
+          </div>
+        </div>
+        <div className="cards">
+          <div className="cards-btn">
+            <img
+              className="cards-img"
+              src={tendencia}
+              alt="Mulher com uma caneta na boca e uma paleta de cores na mão"
+            />
+            <span className="subtitles">TENDÊNCIA</span>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Looks;
